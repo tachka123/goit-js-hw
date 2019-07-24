a7050fbe073607ed9c14b0069e156cb091bdcbed
Sasha Savchuk <Sasha>
2019-07-24 18:22:47 +0300

    g

--- module-1/task-6/script.js
+++ module-1/task-6/script.js
@@ -1,16 +1,13 @@
-"use strict";
-
 let input;
 let total = 0;
 for (i = 0; i != null; i++) {
 	input = prompt(`ваше число ${total}, сколько прибавим еще?`);
-	total += Number(input);
-	if (input == null  ) {
+	total += Number.parseInt(input);
+	if (input === null  ) {
 		alert(`Общая сумма чисел ${total}`);
 		break;
 	}
-	else if (isNaN(input) === true){
+	else if (isNaN(input)){
 		alert('Запрещенно вводить строки! Попробуйте еще раз');
-		break;
 	}
 }
