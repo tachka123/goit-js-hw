07802eace3001efc54729f720d6d5972e12d6304
Sasha Savchuk <Sasha>
2019-10-10 22:18:50 +0300

    fixes

--- goit-js-hw-10/src/js/themeSet.js
+++ goit-js-hw-10/src/js/themeSet.js
@@ -2,14 +2,16 @@ import { eventListener, switcher } from './themeEventListener';
 import Theme from '../data/theme';
 
 export default function() {
-  if (localStorage.getItem('theme') !== null) {
-    const currentTheme = localStorage.getItem('theme');
-    document.querySelector('body').classList.add(currentTheme);
+  const currentTheme = localStorage.getItem('theme');
+  const { body } = document;
+  if (currentTheme !== null) {
+    body.classList.add(currentTheme);
     if (currentTheme === Theme.DARK) {
       switcher.checked = true;
     }
   } else {
     localStorage.setItem('theme', Theme.LIGHT);
+    body.classList.add(Theme.LIGHT);
   }
   eventListener();
 }
