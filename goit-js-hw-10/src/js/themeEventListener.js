56fc6ab78841e86bdd0b03cfc8538399ee42c0b0
Sasha Savchuk <Sasha>
2019-10-10 22:10:26 +0300

    fix

--- goit-js-hw-10/src/js/themeEventListener.js
+++ goit-js-hw-10/src/js/themeEventListener.js
@@ -6,18 +6,15 @@ function eventListener() {
 }
 const switcher = document.getElementById('theme-switch-control');
 const body = document.querySelector('body');
-body.classList.add('light-theme');
 
 function changeTheme() {
   if (switcher.checked) {
     body.classList.remove(Theme.LIGHT);
     body.classList.add(Theme.DARK);
-    localStorage.clear();
     localStorage.setItem('theme', Theme.DARK);
   } else {
     body.classList.remove(Theme.DARK);
     body.classList.add(Theme.LIGHT);
-    localStorage.clear();
     localStorage.setItem('theme', Theme.LIGHT);
   }
 }
--- goit-js-hw-10/src/js/themeSet.js
+++ goit-js-hw-10/src/js/themeSet.js
@@ -2,14 +2,14 @@ import { eventListener, switcher } from './themeEventListener';
 import Theme from '../data/theme';
 
 export default function() {
-  try {
+  if (localStorage.getItem('theme') !== null) {
     const currentTheme = localStorage.getItem('theme');
     document.querySelector('body').classList.add(currentTheme);
     if (currentTheme === Theme.DARK) {
       switcher.checked = true;
     }
-  } catch {
-    localStorage.setItem('theme', theme.light);
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT);
   }
   eventListener();
 }
